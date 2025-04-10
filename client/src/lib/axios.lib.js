10f01a4f1cda1630724f9ef03b6043137b880934
sura721 <you@example.com>
2025-04-10 17:54:54 -0300

    backend

--- client/src/lib/axios.lib.js
+++ client/src/lib/axios.lib.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api", 
+  baseURL: "https://portfolio-yboi.onrender.com/api", 
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
