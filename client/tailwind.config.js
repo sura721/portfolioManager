7b9c8e1dd304e864fc1ac2c7722e5096a1c30203
sura721 <you@example.com>
2025-04-10 17:39:25 -0300

    tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,7 @@ import tailwind from "tailwindcss"
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", // Adjust paths to match your project
+    "./src/**/*.{js,jsx,ts,tsx}", 
   ],
   theme: {
     extend: {},
